e2c449db1a36a2152a415f03f71dd87794a248cd
faustus-gomes <faustus.gomes@gmail.com>
2025-01-30 19:57:11 -0300

    Primeiro Commit

---
+++ src/main/java/jdev/lojavirtual_fs/lojavirtual_fs/LojavirtualFsApplication.java
@@ -0,0 +1,14 @@
+package jdev.lojavirtual_fs.lojavirtual_fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualFsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LojavirtualFsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jdev/lojavirtual_fs/lojavirtual_fs/LojavirtualFsApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.lojavirtual_fs.lojavirtual_fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
