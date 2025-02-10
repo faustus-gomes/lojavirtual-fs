b19a59f7cf7c1fa66bedbb3dc4faad91de4051ba
faustus-gomes <faustus.gomes@gmail.com>
2025-02-10 18:20:50 -0300

    Criação modelo Acesso, Endereco, Pessoa, PessoaJuridica, PessoaFisica

---
+++ src/main/java/jdev/lojavirtual_fs/lojavirtual_fs/model/Acesso.java
@@ -0,0 +1,63 @@
+package jdev.lojavirtual_fs.lojavirtual_fs.model;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1,allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+    @Column(nullable = false)
+    private String descricao; /*Acesso ex: Role_Admin ou Role_secretario*/
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Acesso other = (Acesso) obj;
+        if (other.id != null)
+            return false;
+        else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
---
+++ src/main/java/jdev/lojavirtual_fs/lojavirtual_fs/model/Endereco.java
@@ -0,0 +1,124 @@
+package jdev.lojavirtual_fs.lojavirtual_fs.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "endereco")
+@SequenceGenerator(name = "seq_endereco", sequenceName = "seq_endereco", allocationSize = 1, initialValue = 1)
+public class Endereco implements Serializable {
+    private static final long erialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_endereco")
+    private Long id;
+
+    @Column(nullable = false)
+    private String ruaLogra;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String numero;
+    private String complemento;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String uf;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRuaLogra() {
+        return ruaLogra;
+    }
+
+    public void setRuaLogra(String ruaLogra) {
+        this.ruaLogra = ruaLogra;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Endereco endereco)) return false;
+        return Objects.equals(getId(), endereco.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
