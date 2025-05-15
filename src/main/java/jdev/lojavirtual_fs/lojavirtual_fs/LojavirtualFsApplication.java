61cef013f837e89fc16fb319c1873b07215e3ab0
faustus-gomes <faustus.gomes@gmail.com>
2025-05-15 14:58:03 -0300

    Criação models: CupDesc, ImagemProduto,NotaFiscalCompra e NotaItemProduto

--- src/main/java/jdev/lojavirtual_fs/lojavirtual_fs/LojavirtualFsApplication.java
+++ src/main/java/jdev/lojavirtual_fs/lojavirtual_fs/LojavirtualFsApplication.java
@@ -1,7 +1,9 @@
 package jdev.lojavirtual_fs.lojavirtual_fs;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class LojavirtualFsApplication {
