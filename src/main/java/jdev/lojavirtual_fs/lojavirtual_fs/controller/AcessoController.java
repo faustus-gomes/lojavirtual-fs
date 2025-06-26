d85c117a1f1f6407ea2ef998d00f66e2f1a4c78b
faustus-gomes <faustus.gomes@gmail.com>
2025-06-26 10:47:45 -0300

    Ajuste e permissão no WebConfigSecurity e Postman

--- src/main/java/jdev/lojavirtual_fs/lojavirtual_fs/controller/AcessoController.java
+++ src/main/java/jdev/lojavirtual_fs/lojavirtual_fs/controller/AcessoController.java
@@ -3,15 +3,25 @@ package jdev.lojavirtual_fs.lojavirtual_fs.controller;
 import jdev.lojavirtual_fs.lojavirtual_fs.model.Acesso;
 import jdev.lojavirtual_fs.lojavirtual_fs.service.AcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class AcessoController {
     @Autowired
     private AcessoService acessoService;
-    @PostMapping("/salvarAcesso")
-    public Acesso salvarAcesso(Acesso acesso) {
-        return acessoService.save(acesso);
+    @ResponseBody
+    @PostMapping(value = "/salvarAcesso")
+    public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
+
+        Acesso acessoSalvo = acessoService.save(acesso);
+
+        return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
     }
 }
---
+++ src/main/java/jdev/lojavirtual_fs/lojavirtual_fs/security/WebConfigSecurity.java
@@ -0,0 +1,32 @@
+package jdev.lojavirtual_fs.lojavirtual_fs.security;
+
+import jakarta.servlet.http.HttpSessionListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true,securedEnabled = true)
+public class WebConfigSecurity implements HttpSessionListener {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth-> auth
+                .requestMatchers(HttpMethod.GET, "/salvarAcesso").permitAll()
+                .requestMatchers(HttpMethod.POST, "/salvarAcesso").permitAll()
+                .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());// Outras configurações (crsf, formLogin, Etc)...
+        return http.build();
+    }
+
+}
