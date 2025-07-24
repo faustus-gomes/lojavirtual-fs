597dd332667ebab4e76508eb59e23e430ddfe73e
faustus-gomes <faustus.gomes@gmail.com>
2025-07-24 15:54:54 -0300

    Adicionando Flyway parte 1

--- src/test/java/jdev/lojavirtual_fs/lojavirtual_fs/LojavirtualFsApplicationTests.java
+++ src/test/java/jdev/lojavirtual_fs/lojavirtual_fs/LojavirtualFsApplicationTests.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -25,6 +26,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@Profile("dev")
 @SpringBootTest(classes = LojavirtualFsApplication.class)
 //@WebMvcTest(AcessoController.class)
 public class LojavirtualFsApplicationTests {
